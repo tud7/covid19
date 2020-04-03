0065a428e604fe6422dc5610f57a21d91b88e0f5
tud7 <tduongcs@gmail.com>
2020-04-02 23:44:40 -0700

    added archiving data into csv file after successfully reading, improve here and there

--- explore_who_covid19_data.py
+++ explore_who_covid19_data.py
@@ -18,6 +18,7 @@ Author: Tu Duong
 Copyright © 2020 Tu Duong All Rights Reserved
 '''
 
+import os
 import csv
 import urllib
 import urllib.request
@@ -30,41 +31,45 @@ import pandas            as pd
 # In[2]:
 
 
-cases_vs_deaths_out = widgets.Output()
-new_cases_out       = widgets.Output()
-
-
-# In[3]:
-
-
-def load_data(url, archived_csv=None):
-    '''
-    Function to download latest data from the WHO URL and load into the data frame
-    '''
+def load_data(url, archived_csv='latest_fulldata_archived.csv'):
+    """Function to load data from the URL into the pandas data frame
+    
+    Keyword arguments:
+    url          -- URL to read the data from
+    archived_csv -- [optional] path to the archive file in case we can't read 
+                    the data from the provided URL
+    """
     
     try:
         df = pd.read_csv(url)
         
-        #TODO: archive csv file if successfully read for offline use
+        # Archive to csv file after successfully read for future offline use
+        df.to_csv(archived_csv)
         
     except urllib.error.HTTPError as ex:
         print('WARNING: Unable to retrieve data...')
         print(' - INFO:', ex)
         
-        if archived_csv is not None:
+        if os.path.exists(archived_csv):
             df = pd.read_csv(archived_csv)
             print(' - INFO: Loading the archived data: ', archived_csv)
 
     return df
 
 
-# In[4]:
+# In[3]:
 
 
-def plot_data(selected_location, yxs, output_widget, title=''):
-    '''
-    Function to plot data based on the selected location
-    '''
+def plot_data(selected_location, yxs, output_widget, title='', kind='line'):
+    """ Function to plot data based on the provided arguments
+    
+    Keyword arguments:
+    selected_location -- the selected location
+    yxs               -- list of all interested data to include in this plot
+    output_widget     -- the output widget to display the plot
+    title             -- optional title of the plot
+    kind              -- plot kind, default to 'line'
+    """
     with output_widget:
         
         output_widget.clear_output(wait=True)
@@ -78,64 +83,71 @@ def plot_data(selected_location, yxs, output_widget, title=''):
         plt.title(title)
         
         for y in yxs:
-            _data.plot(kind='line', x='date', y=y,  ax=ax)
+            _data.plot(kind=kind, x='date', y=y,  ax=ax)
 
         plt.grid()
         plt.show()
 
 
-# In[5]:
+# In[4]:
 
 
-def on_location_change(change):
+def on_location_change1(change):
     '''
-    Function will be called when user select the new value in the dropdown list
+    Function will be called when user select the new value in the dropdown list #1
     '''
     
     selected_location = change.new
-    plot_data(selected_location, ['total_cases', 'total_deaths'], cases_vs_deaths_out, "Covid-19 Total Cases vs. Total Deaths")
+    plot_data(selected_location, ['total_cases', 'total_deaths'], cases_vs_deaths_out, title="Covid-19 Total Cases vs. Total Deaths")
     
 
 def on_location_change2(change):
     '''
-    Function will be called when user select the new value in the dropdown list
+    Function will be called when user select the new value in the dropdown list #2
     '''
     
     selected_location = change.new
-    plot_data(selected_location, ['new_cases'], new_cases_out, "Covid-19 Day-by-Day New Cases")
+    plot_data(selected_location, ['new_cases'], new_cases_out, title="Covid-19 Day-by-Day New Cases", kind="bar")
+
+
+# In[5]:
+
+
+# load data into pandas data frame
+covid19_fulldata_url = "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
+df = load_data(url=covid19_fulldata_url)
+
+cases_vs_deaths_out = widgets.Output()
+new_cases_out       = widgets.Output()
+default_location    = "United States"
 
 
 # In[6]:
 
 
-# load data into pandas data frame
-covid19_fulldata_url = "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
-df = load_data(url=covid19_fulldata_url, archived_csv="who_covid19_data_20200317.csv")
 
 
-# In[7]:
-
-
-default_location = "World"
+# Plot Total Cases vs Total Deaths
 
 # the dropdown list allows user to select the location
-location_dropdown = widgets.Dropdown(
+location_dropdown1 = widgets.Dropdown(
     options=df.location.unique(),
     value=default_location,
     description='Location:',
     disabled=False,
 )
-location_dropdown.observe(on_location_change, names='value')
+# Set the callback function when the value changed.
+location_dropdown1.observe(on_location_change1, names='value')
 
 plot_data(default_location, ['total_cases', 'total_deaths'], cases_vs_deaths_out, "Covid-19 Total Cases vs. Total Deaths")
-display(location_dropdown)
+display(location_dropdown1)
 display(cases_vs_deaths_out)
 
 
-# In[8]:
+# In[7]:
 
 
-default_location2 = "World"
+# Plot Daily New Cases
 
 # the dropdown list allows user to select the location
 location_dropdown2 = widgets.Dropdown(
@@ -146,7 +158,7 @@ location_dropdown2 = widgets.Dropdown(
 )
 location_dropdown2.observe(on_location_change2, names='value')
 
-plot_data(default_location, ['new_cases'], new_cases_out, "Covid-19 Day-by-Day New Cases")
+plot_data(default_location, ['new_cases'], new_cases_out, title="Covid-19 Day-by-Day New Cases", kind='bar')
 display(location_dropdown2)
 display(new_cases_out)
 
